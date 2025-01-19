180e464c4ada57d460b5170627efca0b8b2f9690
Homele33 <ontheedge101@gmail.com>
2025-01-19 11:30:52 +0200

    separate the GUI and logic into Taskmaster and TaskList

--- TaskList.py
+++ TaskList.py
@@ -1,61 +1,37 @@
-import customtkinter as ctk
-import tkinter as tk
+import json
 from Task import Task
-from tkinter import ttk
+from TaskTreeview import TaskTreeview
 
-# class TaskList(ctk.CTkScrollableFrame):
-#     def __init__(self, master, title, values):
-#         super().__init__(master,label_text=title)
-#         self.title = title
-#         self.values = values
-#         self.grid_columnconfigure(0, weight=1)
-#         self.tasks = []
-#
-#         for i, value in enumerate(self.values):
-#             task =  ctk.CTkCheckBox(self, text=value)
-#             task.grid(row=i, column=0, sticky="w")
-#             self.tasks.append(task)
-#
-#     def get_selected_tasks(self):
-#         selected_tasks = []
-#         for task in self.tasks:
-#             if task.get() == 1:
-#                 selected_tasks.append(task.cget("text"))
-#         return selected_tasks
-
-
-class TaskFrame(ctk.CTkFrame):
-    def __init__(self, master, tasks):
-        super().__init__(master)
-        self.master = master
-        self.tasks = tasks
-
-        # treeview customization
-        background_color = self._apply_appearance_mode(ctk.ThemeManager.theme["CTkFrame"]["fg_color"])
-        text_color = self._apply_appearance_mode(ctk.ThemeManager.theme["CTkLabel"]["text_color"])
-        selected_color = self._apply_appearance_mode(ctk.ThemeManager.theme["CTkButton"]["fg_color"])
-        # define the style
-        tree_style = ttk.Style()
-        tree_style.theme_use("default")
-        tree_style.configure("Treeview", background=background_color, fieldbackground=background_color, foreground=text_color)
-        tree_style.map("Treeview", background=[('selected', selected_color)])
-        self.bind("<<TreeviewSelect>>", lambda event: self.focus_get())
-
-
-        # treeview widget data
-        self.treeview = ttk.Treeview(master = self.master, show="tree")
-        self.treeview.pack(fill="both", expand=True)
+class TaskList:
+    # TaskList class to manage tasks
+    def __init__(self, master):
+        # create the task treeview
+        self.task_tree = TaskTreeview(master)
+        # define the categories and priorities
+        self.categories = ["Personal", "Work", "School", "Home"]
+        self.priorities = ["High", "Medium", "Low"]
 
         # load in tasks
-        for i, task in enumerate(self.tasks):
-            task_id = self.treeview.insert("","end", text=task.name)
-            if task.subtasks:
-                for j, subtask in enumerate(task.subtasks):
-                    self.treeview.insert(task_id, j, f'{task_id}s{j}', text=subtask.name)
+        self.tasks = self.load_tasks()
+        self.task_tree.load_tasks(self.tasks)
 
     def add_task(self, task: Task):
         # add a new task to the treeview
         self.tasks.append(task)
-        task_id = self.treeview.insert("", "end", text=task.name)
-        for sub_task in task.subtasks:
-            self.treeview.insert(task_id, "end", text=sub_task.name)
+        self.task_tree.add_task(task)
+        self.save_tasks()
+
+    def load_tasks(self):
+        # load in tasks from the tasks.json file
+        try:
+            with open("tasks.json", "r") as file:
+                tasks_data = json.load(file)
+                self.tasks = [Task.from_json(task) for task in tasks_data]
+                return self.tasks
+        except FileNotFoundError:
+            return []
+
+    def save_tasks(self):
+        # save the tasks to the tasks.json file
+        with open("tasks.json", "w") as file:
+            json.dump([task.to_dict() for task in self.tasks], file, indent=4)
--- Taskmaster.py
+++ Taskmaster.py
@@ -1,43 +1,39 @@
 import customtkinter as ctk
 from Task import Task, Subtask
-from TaskList import TaskFrame
-from icecream import ic
+from TaskList import TaskList
 
-class TaskManagerGui:
+
+class Taskmaster:
     # Initialize the TaskManager instance.
     def __init__(self):
         # define the root window.
         self.root = ctk.CTk()
         self.root.title("Taskinator")
         self.root.geometry("1200x800")
+
         # Set appearance mode and color theme
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("blue")
 
-        # todo : move task data organization to a separate class
-        self.tasks = [Task("Task 1"), Task("Task 2"), Task("Task 3")]
-        self.tasks[0].add_subtask(Subtask("Subtask 1"))
-        self.tasks[0].add_subtask(Subtask("Subtask 2"))
-        self.tasks[1].subtasks.append(Subtask("Subtask 3"))
-        self.categories = ["Personal", "Work", "School", "Home"]
-        self.priorities = ["High", "Medium", "Low"]
-
+        # Create the main container frame.
         self.main_container = ctk.CTkFrame(self.root)
         self.main_container.pack(fill="both", expand=True)
 
-
+        # Create the header frame.
         self.create_header()
-        self.create_tabs()
 
+        # Create the tabview to hold the task list and calendar view.
+        self.tabview = self.create_tabs()
+        self.task_list = TaskList(master=self.tabview.tab("Tasks"))
+
+        # Create a new task entry widget.
         self.new_entry = ctk.CTkEntry(self.main_container, placeholder_text="Enter a new task", )
         self.new_entry.pack(side="bottom", expand=False, fill="x", padx=10)
         self.new_entry.bind("<Return>", lambda event: self.add_task())
-        # self.new_entry.grid(row=0, column=0, sticky='W', padx=10, columnspan=2)
-
-
-
+        self.new_entry.focus_set()
 
     def create_header(self):
+        # Create the header frame.
         header_frame = ctk.CTkFrame(self.main_container)
         header_frame.pack(fill="x", expand=False)
 
@@ -49,33 +45,23 @@ class TaskManagerGui:
 
     def create_tabs(self):
         # Create a tabview to hold the task list and calendar view.
-        self.tabview = (ctk.CTkTabview(self.main_container))
-        self.tabview.pack(fill="both", expand=True)
-        # self.tabview.grid(row=0, column=0, sticky="nsew")
-        # Create the task list and calendar tabs.
-        self.task_tabs = self.tabview.add("Tasks")
-        self.calender_tab = self.tabview.add("Calender")
+        tabview = (ctk.CTkTabview(self.main_container))
+        tabview.pack(fill="both", expand=True)
 
-        self.create_task_list()
-        self.create_calendar_view()
+        # Create the task list and calendar tabs.
+        tabview.add("Tasks")
+        tabview.add("Calender")
+        return tabview
 
     def add_task(self):
+        # Add a new task to the task list and to the treeview.
         new_task = Task(self.new_entry.get())
-        ic(new_task.name)
-        self.tasks.append(new_task)
-        self.task_list_frame.add_task(new_task)
+        self.task_list.add_task(new_task)
         self.new_entry.delete(0, "end")
-        # self.reload_task_list()
-
 
     def edit_task(self):
-        selected_item = self.task_list_frame.treeview.selection()
+        selected_item = self.task_list.task_tree.treeview.selection()
         print("Selected item: ", selected_item)
-
-    def create_task_list(self):
-        # Create a grid to hold the task list.
-        self.task_list_frame = TaskFrame(master=self.task_tabs,tasks=self.tasks)
-
 
     def create_calendar_view(self):
         pass
@@ -83,5 +69,5 @@ class TaskManagerGui:
 
 if __name__ == "__main__":
 
-    app = TaskManagerGui()
+    app = Taskmaster()
     app.root.mainloop()
